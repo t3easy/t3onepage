3af14b742f95dc571f6525d664bb2d06ce0341ed
Jan Kiesewetter <jan@t3easy.de>
2013-11-15 13:34:28 +0100

    [TASK] Reformat code

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,32 +1,30 @@
 <?php
-    $EM_CONF[$_EXTKEY] = array(
-        'title' => 't3onepage',
-        'description' => '',
-        'category' => 'templates',
-        'author' => 'Maximilian Mayer',
-        'author_email' => 'maximilian.mayer@compuart.com',
-        'author_company' => 'compuart.com',
-        'shy' => '',
-        'priority' => '',
-        'module' => '',
-        'state' => 'stable',
-        'internal' => '',
-        'uploadfolder' => '0',
-        'createDirs' => '',
-        'modify_tables' => '',
-        'clearCacheOnLoad' => 0,
-        'lockType' => '',
-        'version' => '1.1.0-dev',
-        'constraints' => array(
-            'depends' => array(
-                'cms' => '',
-                'extbase' => '',
-                'fluid' => '',
-            ),
-            'conflicts' => array(
-            ),
-            'suggests' => array(
-            ),
-        ),
-    );
-?>
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 't3onepage',
+	'description' => '',
+	'category' => 'templates',
+	'author' => 'Maximilian Mayer',
+	'author_email' => 'maximilian.mayer@compuart.com',
+	'author_company' => 'compuart.com',
+	'shy' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => '0',
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'version' => '1.1.0-dev',
+	'constraints' => array(
+		'depends' => array(
+			'cms' => '',
+			'extbase' => '',
+			'fluid' => '',
+		),
+		'conflicts' => array(),
+		'suggests' => array(),
+	),
+);
