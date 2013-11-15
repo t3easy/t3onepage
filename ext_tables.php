282250881591977c04161283d003be442b3da678
Jan Kiesewetter <jan@t3easy.de>
2013-11-15 13:53:50 +0100

    [BUGFIX] Include page/user TSconfig

--- ext_tables.php
+++ ext_tables.php
@@ -1,18 +1,18 @@
 <?php
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
 if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-t3lib_extMgm::addStaticFile($_EXTKEY, 'Resources/Private/TypoScript/', 't3onepage');
+ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Resources/Private/TypoScript/', 't3onepage');
 
-// Add page TSConfig
-$pageTsConfig = \TYPO3\CMS\Core\Utility\GeneralUtility::getUrl(
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Resources/Private/TsConfig/Page/config.ts');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig($pageTsConfig);
+	// Add page TSConfig
+ExtensionManagementUtility::addPageTSConfig(
+	'<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Resources/Private/TsConfig/Page/config.ts">'
+);
 
-// Add user TSConfig
-$userTsConfig = \TYPO3\CMS\Core\Utility\GeneralUtility::getUrl(
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Resources/Private/TsConfig/User/config.ts');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig($userTsConfig);
-
-?>
+	// Add user TSConfig
+ExtensionManagementUtility::addUserTSConfig(
+	'<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Resources/Private/TsConfig/User/config.ts">'
+);
