76a6c4eac1610d693ee9c4c683cc758c0d9e2728
Maximilian Mayer <info@maximilian-mayer.net>
2013-11-17 21:09:37 +0100

    [BUGFIX] Add header height to scroll script

--- Resources/Public/JavaScript/jquery.custom.js
+++ Resources/Public/JavaScript/jquery.custom.js
@@ -2,16 +2,18 @@ $(document).ready(function() {
 	
 	// Smooth Scroll
 	$('a[href*=#]').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-        && location.hostname == this.hostname) {
-            var $target = $(this.hash);
-            $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
-            if ($target.length) {
-                var targetOffset = $target.offset().top-105;
-                $('html,body').animate({scrollTop: targetOffset}, 1000);
-                return false;
-            }
-        }
-    });
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+		&& location.hostname == this.hostname) {
+			var $target = $(this.hash);
+			$target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+			if ($target.length) {
+				var headerHeight = $("#header").height()+40;
+				var targetOffset = $target.offset().top-headerHeight;
+				
+				$('html,body').animate({scrollTop: targetOffset}, 1000);
+				return false;
+			}
+		}
+	});
     
 });
