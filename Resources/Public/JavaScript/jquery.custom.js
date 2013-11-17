20fc13804be9397babd8f6a56d39ea9b57f8f1f3
Maximilian Mayer <info@maximilian-mayer.net>
2013-11-17 15:57:32 +0100

    [FEATURE] Fixed header while scrolling

--- Resources/Public/JavaScript/jquery.custom.js
+++ Resources/Public/JavaScript/jquery.custom.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
             var $target = $(this.hash);
             $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
             if ($target.length) {
-                var targetOffset = $target.offset().top;
+                var targetOffset = $target.offset().top-105;
                 $('html,body').animate({scrollTop: targetOffset}, 1000);
                 return false;
             }
