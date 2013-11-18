7792b9385a2146a46487a63687dd160b94d0171e
Jan Kiesewetter <jan@t3easy.de>
2013-11-18 13:55:19 +0100

    [BUGFIX] User full qualified class name to prevent conflicts in cache

--- ext_tables.php
+++ ext_tables.php
@@ -1,18 +1,19 @@
 <?php
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Resources/Private/TypoScript/', 't3onepage');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+	$_EXTKEY, 'Resources/Private/TypoScript/', 't3onepage'
+);
 
 	// Add page TSConfig
-ExtensionManagementUtility::addPageTSConfig(
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
 	'<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Resources/Private/TsConfig/Page/config.ts">'
 );
 
 	// Add user TSConfig
-ExtensionManagementUtility::addUserTSConfig(
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
 	'<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Resources/Private/TsConfig/User/config.ts">'
 );
