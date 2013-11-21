83ce078b356aed30cc4d5f61dedcf734673005fa
Jan Kiesewetter <jan@t3easy.de>
2013-11-21 11:49:34 +0100

    [TASK] Make loading TSconfig configurable in extension manager

--- ext_tables.php
+++ ext_tables.php
@@ -4,16 +4,22 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
+$configuration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
 	$_EXTKEY, 'Resources/Private/TypoScript/', 't3onepage'
 );
 
 	// Add page TSConfig
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-	'<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Resources/Private/TsConfig/Page/config.ts">'
-);
+if ($configuration['addPageTSConfig'] == TRUE) {
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+		'<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Resources/Private/TsConfig/Page/config.ts">'
+	);
+}
 
 	// Add user TSConfig
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
-	'<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Resources/Private/TsConfig/User/config.ts">'
-);
+if ($configuration['addUserTSConfig'] == TRUE) {
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
+		'<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Resources/Private/TsConfig/User/config.ts">'
+	);
+}
