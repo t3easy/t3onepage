8171de3dc37aa4c200ab164fd4d42b1ec283a1a2
Jan Kiesewetter <jan@t3easy.de>
2014-04-02 22:16:49 +0200

    [TASK] Include logo with path function

--- Configuration/TypoScript/Library/lib.logo.ts
+++ Configuration/TypoScript/Library/lib.logo.ts
@@ -1,12 +1,13 @@
 lib.logo = COA
 lib.logo {
-	10 = TEXT 
+	10 = TEXT
 	10 {
 		value (
-<object class="logo" data="/typo3conf/ext/t3onepage/Resources/Public/Images/typo3_sign.svg" type="image/svg+xml">
-    <div>TYPO3 OnePage Template</div>
+<object class="logo" data="{PATH:EXT:t3onepage/Resources/Public/Images/typo3_sign.svg}" type="image/svg+xml">
+	<div>TYPO3 OnePage Template</div>
 </object>
 		)
+		insertData = 1
 	}
 
 	# wrap = <div class="logo">|</div>
